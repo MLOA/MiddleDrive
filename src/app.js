8272258b6a78b07a8f48cc7fc057eaab831074c2
DESKTOP-L9CGUSF\shiso <nooba704@gmail.com>
2017-12-01 14:42:20 +0900

    構造を分けた

---
+++ src/app.js
@@ -0,0 +1,41 @@
+'use strct'
+
+import electron from 'electron'
+import express from 'express'
+import bodyParser from 'body-parser'
+import controller from './controllers'
+
+const port = (process.env.PORT || 3000)
+
+const exapp = express()
+exapp.use(express.static('public'))
+exapp.use(bodyParser.json())
+exapp.use(bodyParser.urlencoded({
+  extended: true
+}))
+exapp.listen(port, '127.0.0.1')
+
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+let mainWindow = null
+
+// 全てのウィンドウが閉じたら終了
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') {
+    app.quit()
+  }
+})
+
+// Electronの初期化完了後に実行
+app.on('ready', () => {
+  //ウィンドウサイズを1280*720（フレームサイズを含まない）に設定する
+  mainWindow = new BrowserWindow({width: 600, height: 400, useContentSize: true})
+
+  //mainWindow.webContents.openDevTools()
+  mainWindow.loadURL('http://127.0.0.1:' + port + '/')
+
+  // ウィンドウが閉じられたらアプリも終了
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+})
