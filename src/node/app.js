3fa1233b771187145681fa3e2b69b74747e43239
DESKTOP-L9CGUSF\shiso <nooba704@gmail.com>
2017-12-07 16:44:47 +0900

    dbからのデータ読みだして整形&res.send

--- src/node/app.js
+++ src/node/app.js
@@ -7,37 +7,51 @@ const app = express()
 const port = 3000
 
 app
-	.use(express.static('public'))
-	.get('/', (req, res) => {
-	})
-	.get('/send/:text', (req, res) => {
-		const url = 'http://localhost:8000/' + req.params.text
-		return fetch(url, {
-			method: 'GET',
-			mode: 'cors'
-		}).then(result => {
-			return result.text()
-		}).then(text => {
-			res.send(text)
-		})
-	})
-	.get('/check/', (req, res) => {
-		// console.log(process.cwd())
-		const sqlite3 = require('sqlite3').verbose()
-		const db = new sqlite3.Database('//rt-ac3200-b250/sda1/client/middle_drive.db')
-		db.serialize(() => {
-			db.all('SELECT * FROM text WHERE id = (select max(id) from text)', (err, rows) => {
-				const rowsObj = rows.map(row => {
-					return {
-						time: row.datetime,
-						text: decodeURIComponent(row.line)
-					}
-				})
-				res.send(rowsObj[0])
-			})
-		})
-		db.close()
-	})
-	.listen(port, () => {
-		console.log(`URL -> http://localhost:${port}/`)
-	})
+  .use(express.static('public'))
+  .get('/', (req, res) => {
+  })
+  .get('/send/:text', (req, res) => {
+    const url = 'http://localhost:8000/' + req.params.text
+    return fetch(url, {
+      method: 'GET',
+      mode: 'cors'
+    }).then(result => {
+      return result.text()
+    }).then(text => {
+      res.send(text)
+    })
+  })
+  .get('/check/', (req, res) => {
+    // console.log(process.cwd())
+    const sqlite3 = require('sqlite3').verbose()
+    const db = new sqlite3.Database('C:/Users/shiso/Documents/Middole-Drive/dist/middle_drive.db')
+    db.serialize(() => {
+      let sql = 'SELECT * FROM texts WHERE id = (select max(id) from texts)'
+      db.all(sql, (err, rows) => {
+        const rowsObj = rows.map(row => {
+          const textObj = decodeURIComponent(row.text).split(/\r?\n/g)
+            .map((text, i) => {
+              return {
+                'line' : i + 1,
+                'text' : text
+              }
+            })
+
+          return {
+            'time'    : '20171208120000',
+            'cursors' :
+              [{'line': 1,
+                'column': 3
+              }],
+            'lines'   : textObj,
+            'device'  : 'MLOA-PC'
+          }
+        })
+        res.send(rowsObj[0])
+      })
+    })
+    db.close()
+  })
+  .listen(port, () => {
+    console.log(`URL -> http://localhost:${port}/`)
+  })
