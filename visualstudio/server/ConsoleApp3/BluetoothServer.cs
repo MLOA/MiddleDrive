15cf66619af20934b5b8848754274ab6d1c49369
DESKTOP-QJEGDBJ\hiroaki <salmon2073@gmail.com>
2017-12-08 20:40:04 +0900

    bluetoothサーバとクライアントを一つにまとめた

--- visualstudio/server/ConsoleApp3/BluetoothServer.cs
+++ visualstudio/server/ConsoleApp3/BluetoothServer.cs
@@ -16,36 +16,55 @@ namespace MiddleDriveServer {
         String dbName = "Data Source=middle_drive.db";
 
         public async Task init() {
-            using (var con = new SQLiteConnection(dbName)) {
-                con.Open();
+            createDB();
+            await initBluetoothServer();
+            initHttpServer();
+            Console.WriteLine("Server start");
+        }
 
-                using (var cmd = con.CreateCommand()) {
-                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS text(ID INTEGER PRIMARY KEY AUTOINCREMENT, datetime INT, line TEXT) ";
-                    cmd.ExecuteNonQuery();
-                }
-            }
+        void createDB() {
+            var con = new SQLiteConnection(dbName);
+            con.Open();
+            var cmd = con.CreateCommand();
+            cmd.CommandText = "CREATE TABLE IF NOT EXISTS text(ID INTEGER PRIMARY KEY AUTOINCREMENT, datetime INT, line TEXT) ";
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
 
+        async Task initBluetoothServer() {
             _provider = await RfcommServiceProvider.CreateAsync(RfcommServiceId.ObexObjectPush);
-
             StreamSocketListener listener = new StreamSocketListener();
             listener.ConnectionReceived += OnConnectionReceived;
-
-            await listener.BindServiceNameAsync(
-                _provider.ServiceId.AsString(),
-                SocketProtectionLevel.BluetoothEncryptionAllowNullAuthentication);
-
+            await listener.BindServiceNameAsync(_provider.ServiceId.AsString(), SocketProtectionLevel.BluetoothEncryptionAllowNullAuthentication);
             _provider.StartAdvertising(listener);
-            Console.WriteLine("Server start");
-            while (true) { };
         }
 
         void OnConnectionReceived(StreamSocketListener listener, StreamSocketListenerConnectionReceivedEventArgs args) {
-            Console.WriteLine("connected");
             var _socket = args.Socket;
             reader = new DataReader(_socket.InputStream);
+            reader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
             writer = new DataWriter(_socket.OutputStream);
+            writer.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+            Console.WriteLine("connected");
             receive();
-            sendHello();
+        }
+
+        async void initHttpServer() {
+            // サーバの開始
+            HttpListener httplistener = new HttpListener();
+            httplistener.Prefixes.Add("http://localhost:8000/");
+            httplistener.Start();
+
+            //接続処理
+            while (true) {
+                var context = httplistener.GetContext();
+                var res = context.Response;
+                var req = context.Request;
+                var param = new StreamReader(req.InputStream, Encoding.GetEncoding("utf-8")).ReadToEnd();
+                await send(param);
+                res.StatusCode = 200;
+                res.Close();
+            }
         }
 
         async Task send(String text) {
@@ -55,53 +74,26 @@ namespace MiddleDriveServer {
             await writer.StoreAsync();
         }
 
-        async void sendHello() {
-            try {
-                HttpListener httplistener = new HttpListener();
-                httplistener.Prefixes.Add("http://localhost:8000/");
-                httplistener.Start();
-                while (true) {
-                    var context = httplistener.GetContext();
-                    var res = context.Response;
-                    var req = context.Request;
-                    var param = "";
-                    using (var reader = new StreamReader(req.InputStream, Encoding.GetEncoding("utf-8"))) {
-                        param = reader.ReadToEnd();
-                    }
-
-                    await send(param);
-                    res.StatusCode = 200;
-                    res.Close();
-                }
-            } catch (Exception ex) {
-                Console.WriteLine("Error: " + ex.Message);
-            }
-        }
-
         async void receive() {
-            try {
-                reader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+            while (true) {
+                //読み出し
                 await reader.LoadAsync(sizeof(int));
                 var length = reader.ReadInt32();
                 await reader.LoadAsync((uint)length);
                 var text2 = reader.ReadString((uint)length);
                 Console.WriteLine("R:" + text2);
-                using (var con = new SQLiteConnection(dbName)) {
-                    con.Open();
 
-                    using (var cmd = con.CreateCommand()) {
-                        cmd.CommandText = "INSERT INTO text (datetime, line) VALUES (@p_datetime, @p_line)";
-                        var time = DateTime.Now.ToString("yyyyMMddHHmmssfff");
-                        cmd.Parameters.Add(new SQLiteParameter("@p_datetime", time));
-                        cmd.Parameters.Add(new SQLiteParameter("@p_line", text2));
-                        cmd.ExecuteNonQuery();
-                    }
-                }
-                receive();
-            } catch (Exception ex) {
-                Console.WriteLine("Error: " + ex.Message);
+                //DBに保存
+                var con = new SQLiteConnection(dbName);
+                con.Open();
+                var cmd = con.CreateCommand();
+                cmd.CommandText = "INSERT INTO text (datetime, line) VALUES (@p_datetime, @p_line)";
+                var time = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                cmd.Parameters.Add(new SQLiteParameter("@p_datetime", time));
+                cmd.Parameters.Add(new SQLiteParameter("@p_line", text2));
+                cmd.ExecuteNonQuery();
+                con.Close();
             }
         }
-
     }
 }
